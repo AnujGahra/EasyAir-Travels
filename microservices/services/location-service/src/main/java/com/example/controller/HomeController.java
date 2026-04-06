48eed756ce61ffee1060a7fe68f64dcb6dfa9d0b
Anuj Kumar <anujgahra244304@gmail.com>
2026-04-06 13:11:12 +0530

    Add common ApiResponse for all services

--- microservices/services/location-service/src/main/java/com/example/controller/HomeController.java
+++ microservices/services/location-service/src/main/java/com/example/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.example.controller;
 
 
+import com.example.payload.response.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+
+
+
     @GetMapping
-    public String HomeController(){
-        return "Hello I am location service of airline ticket booking System";
+    public ApiResponse HomeController(){
+
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setMessage("Hello I am location service of airline ticket booking System");
+        return apiResponse;
     }
 }
