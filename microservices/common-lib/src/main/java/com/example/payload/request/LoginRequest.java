046e4c4c60b86fdfc3a2590cde32268424adb0f2
Anuj Kumar <anujgahra244304@gmail.com>
2026-04-09 02:01:33 +0530

    feat(user-service): add AuthController to handle user registration and login with JWT-based authentication

---
+++ microservices/common-lib/src/main/java/com/example/payload/request/LoginRequest.java
@@ -0,0 +1,6 @@
+package com.example.payload.request;
+
+public class LoginRequest {
+
+    
+}
---
+++ microservices/services/user-service/src/main/java/com/example/controller/AuthController.java
@@ -0,0 +1,41 @@
+package com.example.controller;
+
+
+import com.example.dto.UserDTO;
+import com.example.payload.request.LoginRequest;
+import com.example.payload.response.AuthResponse;
+import com.example.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> signup(
+            @RequestBody @Valid UserDTO userDTO
+    ) throws Exception {
+        AuthResponse response = authService.signup(userDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody @Valid LoginRequest req) throws UserException {
+        AuthResponse response = authService.login(req.getEmail(), req.getPassword());
+        return ResponseEntity.ok(response);
+    }
+
+
+}
